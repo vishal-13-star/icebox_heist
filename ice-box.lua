85f84af3bd7508c3dcfab350f1fb11a64de0f807
vishal-13-star <bavaniya.vishal.2003@gmail.com>
2025-09-10 15:51:49 +0530

    Add files via upload

---
+++ ice-box.lua
@@ -0,0 +1,42 @@
+
+
+-- main left side created by bavan
+Config.DoorList['ice-box-main left side'] = {
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 1,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -770610628,
+    objCoords = vec3(-1230.656738, -800.096558, 18.008320),
+    fixText = false,
+    objYaw = 307.11312866211,
+}
+
+-- main right side created by bavan
+Config.DoorList['ice-box-main right side'] = {
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 1,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -770610628,
+    objCoords = vec3(-1232.228271, -798.019653, 18.008320),
+    fixText = false,
+    objYaw = 127.1131362915,
+}
+
+-- 2nd door created by bavan
+Config.DoorList['ice-box-2nd door'] = {
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 1,
+    objCoords = vec3(-1258.020142, -820.688293, 17.181341),
+    objYaw = 217.1131439209,
+    objName = -506110411,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+}
